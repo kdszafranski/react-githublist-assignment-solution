ed122342dbdca4eea8bcd471d062f71deed7881e
Kris Szafranski <kris@thoughtshelter.com>
2018-02-14 08:31:40 -0600

    completed get request

--- src/App.js
+++ src/App.js
@@ -13,12 +13,36 @@ class App extends Component {
 
     // Give our function access to `this`
     this.addStudent = this.addStudent.bind(this);
+
+    this.getStudents();
   }
 
   // This function is called by the StudentForm when the submit button is pressed
   addStudent(newStudent) {
     console.log(newStudent);
     // POST your data here
+    axios.post('/students', newStudent)
+      .then((response) => {
+        // console.log(response);    
+        this.getStudents();    
+      })
+      .catch((error) => {
+        console.log('error posting: ', error);
+      });
+  }
+
+  getStudents() {
+    console.log('here');
+    
+    axios.get('/students')
+      .then((response) => {
+        console.log(response.data);
+        // just give it the object you want to store
+        this.setState({ studentList: response.data });
+      })
+      .catch((error) => {
+        console.log('error get students: ', error);
+      });
   }
 
   render() {
